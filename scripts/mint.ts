daf8343984058ae8d503ae895d8a1381eb17ef36
kyoungwan.chae <kyngwan@gmail.com>
2023-08-31 23:31:37 +0900

    Hardcode reward as 0.01TON

--- scripts/mint.ts
+++ scripts/mint.ts
@@ -14,7 +14,7 @@ export async function run(provider: NetworkProvider) {
     await nftCollection.send(
         provider.sender(),
         {
-            value: toNano('0.3'),
+            value: toNano('0.1'), // 0.1 TON TODO: Set 1 TON in hackathon
         },
         'Mint'
     );
--- scripts/setReward.ts
+++ scripts/setReward.ts
@@ -1,28 +1,28 @@
-import { Address, toNano } from 'ton-core';
-import { NftItem } from '../wrappers/NftItem';
-import { NetworkProvider } from '@ton-community/blueprint';
-import { NftCollection } from '../wrappers/NftCollection';
+// import { Address, toNano } from 'ton-core';
+// import { NftItem } from '../wrappers/NftItem';
+// import { NetworkProvider } from '@ton-community/blueprint';
+// import { NftCollection } from '../wrappers/NftCollection';
 
-export async function run(provider: NetworkProvider) {
-    const nftCollection = provider.open(await NftCollection.fromInit());
-    const ownerAddress = provider.sender().address;
-    if (!ownerAddress) {
-        return;
-    }
-    const nftItemAddress = await nftCollection.getGetMyNftAddress(ownerAddress);
-    if (!nftItemAddress) {
-        return;
-    }
-    const nftItem = provider.open(await NftItem.fromAddress(nftItemAddress));
+// export async function run(provider: NetworkProvider) {
+//     const nftCollection = provider.open(await NftCollection.fromInit());
+//     const ownerAddress = provider.sender().address;
+//     if (!ownerAddress) {
+//         return;
+//     }
+//     const nftItemAddress = await nftCollection.getGetMyNftAddress(ownerAddress);
+//     if (!nftItemAddress) {
+//         return;
+//     }
+//     const nftItem = provider.open(await NftItem.fromAddress(nftItemAddress));
 
-    await nftItem.send(
-        provider.sender(),
-        {
-            value: toNano('0.05'),
-        },
-        {
-            $$type: 'SetReward',
-            reward: toNano('0.001'),
-        }
-    );
-}
+//     await nftItem.send(
+//         provider.sender(),
+//         {
+//             value: toNano('0.05'),
+//         },
+//         {
+//             $$type: 'SetReward',
+//             reward: toNano('0.001'),
+//         }
+//     );
+// }
